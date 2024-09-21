f01115b6b8351aa3fc73cadb86c73a10bed1b34f
Frank <156254944+funnyfranky@users.noreply.github.com>
2024-09-21 15:59:01 -0600

    Completed assignments 1-4

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.Write("Press enter to continue...");
+        string input = Console.ReadLine();
+        if (input == "")
+        {
+            Console.WriteLine("Nice!");
+        }
+        else
+        {
+            Console.WriteLine($"You typed '{input}' before you hit enter. You're the worst.");
+        }
     }
 }
