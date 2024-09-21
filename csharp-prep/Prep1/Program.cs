e511cc35e4eb787e92232f612fa08bab2d9f473f
Frank <156254944+funnyfranky@users.noreply.github.com>
2024-09-21 15:58:07 -0600

    Completed prep 1 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string FName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string LName = Console.ReadLine();
+
+        Console.Write($"Your name is {LName}, {FName} {LName}.");
     }
 }
