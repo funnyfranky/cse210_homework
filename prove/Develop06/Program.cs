55de2946fff9d12d4cdc12a187c8a6db53837b61
Frank <156254944+funnyfranky@users.noreply.github.com>
2024-10-29 14:35:48 -0600

    added an "exceeds core requirements" paragraph

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -10,3 +10,5 @@ class Program
         manager.Start();
     }
 }
+
+// To exceed core requirements I added helper functions to do things like parse strings and return -1 if it's not a valid string, one that adds an int to the score in goal manager to aid with bonus points, one that's called ExOrNah that helps when I print out the list of goals by returning the correct string based on if it's completed or not. They help simplify my code a little bit.
